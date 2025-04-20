4e3049a1af39cdf59204bd6865b4049190a5eb5f
Ayan-alt-deep <sankarraj5900@gmail.com>
2025-04-20 12:33:05 +0600

    Update session.js

--- session.js
+++ session.js
@@ -5,5 +5,5 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XrJRSaoA#p_nvlK-Mr5zTZAcDC4X9mKYUC-6W1Au7ABXrrALDjRc",
+SESSION_ID: process.env.SESSION_ID || "ANJU-XPRO~jV8nDT7b#sThumM7P5-QAp5RZ3gPcGA9wHtZu_zjYm9pmZ0-lK08",
 };
